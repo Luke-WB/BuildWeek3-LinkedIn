f5c67d159ea516168e529429f924cbb1ff1b19b5
Vallerga <antoniovallerga85@gmail.com>
2023-02-27 12:55:44 +0100

    store e reducer

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,22 @@
+import {
+    CARICA_FETCH,
+  } from "../actions";
+
+const initialState = {
+    profile: {},
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNmZhM2YxOTNlNjAwMTM4MDdmNTkiLCJpYXQiOjE2Nzc0ODg4MTYsImV4cCI6MTY3ODY5ODQxNn0.aQD1NJmhLvpzQEKvINIXWvlSMDQG-S49TU3R9DM5PWs"
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CARICA_FETCH:
+        return {
+            ...state,
+            profile: [action.payload],
+        }
+    default:
+        return state;
+    }
+}
+
+export default reducer
