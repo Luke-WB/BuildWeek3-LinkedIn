bfc7f9d854f52c10a9202c2659afc85412f56e74
IlBennys <119436512+IlBennys@users.noreply.github.com>
2023-02-27 20:02:26 +0100

    fix offcanvas

--- src/components/NavCustom.jsx
+++ src/components/NavCustom.jsx
@@ -1,10 +1,15 @@
 import { Button, Container, Form, Nav, Navbar, NavDropdown, Offcanvas, Card, ListGroup } from "react-bootstrap"
 import logo from "../assets/LinkedIn_logo_initials.png"
-import { FaSearch, FaHome, FaUserFriends, FaBell } from "react-icons/fa"
-import { BsBriefcaseFill, BsGrid3X3GapFill, BsPlayBtnFill } from "react-icons/bs"
-import { RiMessage3Line } from "react-icons/ri"
 import React, { useState } from "react"
 import { useSelector } from "react-redux"
+
+import { FaSearch, FaHome, FaUserFriends, FaBell, FaCompass } from "react-icons/fa"
+import { BsBriefcaseFill, BsGrid3X3GapFill, BsPlayBtnFill } from "react-icons/bs"
+import { CgInsights } from "react-icons/cg"
+import { RiSuitcaseFill, RiAdvertisementLine, RiMessage3Line } from "react-icons/ri"
+import { MdGroups } from "react-icons/md"
+import { TiTick } from "react-icons/ti"
+import { AiOutlinePlus } from "react-icons/ai"
 
 function OffCanvasExample({ name, ...props }, prop) {
   const myProfile = useSelector((state) => state.profile.profile)
@@ -102,29 +107,94 @@ function OffCanvasExample({ name, ...props }, prop) {
                   <Offcanvas.Title>Work</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                  <div>
-                    <h6>Visit More LinkedIn Products</h6>
-                    <p>Learning Insights Post a job Advertise Find Leads Groups Services Marketplace </p>
-                  </div>
-                  <div>
-                    <Card style={{ width: "18rem" }}>
-                      <ListGroup variant="flush">
-                        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                      </ListGroup>
-                    </Card>
-                    <h6>LinkedIn Business Services</h6>
-                    <p>
-                      <BsPlayBtnFill />
-                      Talent Solutions Find, attract and recruit talent
-                    </p>
-                    <p>Sales Solutions Unlock sales opportunities</p>
-                    <p> Post a job for free Get your job in front of quality candidates</p>
-                    <p> Marketing Solutions Acquire customers and grow your business</p>
-                    <p>Learning Solutions Develop talent across your organization</p>
-                  </div>
-                  <div>Create a Company Page</div>
+                  <Card style={{ width: "23rem" }}>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>
+                        <strong>Visit More LinkedIn Products</strong>
+                      </ListGroup.Item>
+                      <ListGroup.Item className="d-flex justify-content-between flex-wrap">
+                        <div className="d-flex flex-column align-items-center ms-2 mia">
+                          <div className="icone" id="banana">
+                            <BsPlayBtnFill />
+                          </div>
+                          Learning
+                        </div>
+                        <div className="d-flex flex-column align-items-center ms-2 mia">
+                          <div className="icone" id="banana">
+                            <CgInsights />
+                          </div>
+                          Insights
+                        </div>
+                        <div className="d-flex flex-column align-items-center ms-2 mia">
+                          <div className="icone" id="banana">
+                            <RiSuitcaseFill />
+                          </div>
+                          Post a job
+                        </div>
+                        <div className="d-flex flex-column align-items-center ms-2 mia">
+                          <div className="icone" id="banana">
+                            <RiAdvertisementLine />
+                          </div>
+                          Advertise
+                        </div>
+                        <div className="d-flex flex-column align-items-center ms-2 mia">
+                          <div className="icone" id="banana">
+                            <FaCompass />
+                          </div>
+                          Find Leads
+                        </div>
+                        <div className="d-flex flex-column align-items-center ms-2 mia">
+                          <div className="icone" id="banana">
+                            <MdGroups />
+                          </div>
+                          Groups
+                        </div>
+                        <div className="d-flex flex-column align-items-center ms-2 mia">
+                          <div className="icone" id="banana">
+                            <TiTick />
+                          </div>
+                          Services Marketplace
+                        </div>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card>
+
+                  <Card className="mt-2" style={{ width: "23rem" }}>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>
+                        <strong>LinkedIn Business Services</strong>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <div>
+                          <strong>Talent Solutions</strong> <p className="mia">Find, attract and recruit talent</p>
+                        </div>
+                        <div>
+                          <strong>Sales Solutions</strong>
+                          <p className="mia"> Unlock sales opportunities</p>
+                        </div>
+                        <div>
+                          <strong>Post a job for free</strong>
+                          <p className="mia">Get your job in front of quality candidates</p>
+                        </div>
+                        <div>
+                          <strong>Marketing Solutions</strong>
+                          <p className="mia">Acquire customers and grow your business</p>
+                        </div>
+                        <div>
+                          <strong>Learning Solutions</strong>
+                          <p className="mia">Develop talent across your organization</p>
+                        </div>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <div>
+                          Create a Company Page
+                          <span style={{ fontSize: "22px" }}>
+                            <AiOutlinePlus />
+                          </span>
+                        </div>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card>
                 </Offcanvas.Body>
               </Offcanvas>
             </Nav>
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,3 @@
-import { useState } from "react"
-
 export const CARICA_FETCH = "CARICA_FETCH"
 
 export const fetchProfile = (token) => {
