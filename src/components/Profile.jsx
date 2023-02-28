4e0832aa06e1f02dd986f8b465c9b1bfa26166a2
Vallerga <antoniovallerga85@gmail.com>
2023-02-28 01:19:53 +0100

    css profile fixed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProfile } from "../redux/actions";
 import { HiUsers } from "react-icons/hi";
@@ -18,149 +18,164 @@ const Profile = () => {
   console.log("random number");
   return (
     <>
-      <div
-        style={{
-          overflow: "hidden",
-          height: "500px",
-          border: "solid 1px grey",
-        }}
-        className="d-flex flex-column align-items-start bg-light rounded-4 my-4 position-relative"
-      >
+      <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
         <div style={{ height: "200px", width: "100%" }}>
+          <a href="#">
+            <img
+              style={{
+                width: "100%",
+                height: "200px",
+                objectFit: "cover",
+                objectPosition: "top",
+              }}
+              src={
+                "https://www.media.inaf.it/wp-content/uploads/2020/03/meteorite-1280x720.jpg"
+              }
+              alt="immagine background"
+            />
+          </a>
+        </div>
+        <a href="#">
           <img
+            className="rounded-circle position-absolute "
             style={{
-              width: "100%",
-              height: "200px",
-              objectFit: "cover",
-              objectPosition: "top",
+              width: 150,
+              top: "100px",
+              left: "25px",
+              border: "solid 4px white",
             }}
             src={
-              "https://www.media.inaf.it/wp-content/uploads/2020/03/meteorite-1280x720.jpg"
+              "https://i.pinimg.com/736x/65/91/a0/6591a0cdc097b089c2b329d1feddee54.jpg"
             }
-            alt="immagine background"
+            alt="immagine profilo"
           />
-        </div>
-        <img
-          className="rounded-circle position-absolute "
-          style={{
-            width: 150,
-            top: "100px",
-            left: "25px",
-            border: "solid 4px white",
-          }}
-          src={
-            "https://i.pinimg.com/736x/65/91/a0/6591a0cdc097b089c2b329d1feddee54.jpg"
-          }
-          alt="immagine profilo"
-        />
+        </a>
         <div className="mt-5 mx-4">
-          <h3 className="mt-2 mb-1">
+          <h2 className="mt-2 mb-0">
             {myProfile.name} {myProfile.surname}
-          </h3>
-          <p className="my-0">{myProfile.title}</p>
-          <p className="proGrey my-0">{myProfile.email}</p>
-          <p className="proGrey">
-            {myProfile.area} -{" "}
+          </h2>
+          <div className="proBlack proMedium proLight">{myProfile.title}</div>
+          <a href="#">
+            <div className="proGrey proLight proGreyHBlue mt-2">
+              {myProfile.email}
+            </div>
+          </a>
+          <div className="mt-2">
+            <a href="#">
+              <span className="proGrey proLight proGreyHBlue">
+                {myProfile.area}
+              </span>
+            </a>{" "}
+            -{" "}
             <a href="#">
               <span className="proBlue">Contact info</span>
             </a>
-          </p>
-          <p className="proBlue">
-            {Math.floor(Math.random() * 100)} connection
-          </p>
-          <Button className="proOpenTo me-2" variant="primary">
-            Open to
-          </Button>
-          <Button className="proModProfile me-2" variant="outline-primary">
-            Add profile section
-          </Button>
-          <Button className="proMore me-2" variant="outline-primary">
-            Add profile section
-          </Button>
+          </div>
+          <a href="#">
+            <div className="proBlue mt-2 mb-3">
+              {Math.floor(Math.random() * 100)} connection
+            </div>
+          </a>
+          <Form className="mb-4">
+            <Button className="proOpenTo me-3" variant="primary">
+              Open to
+            </Button>
+            <Button className="proModProfile me-3" variant="outline-primary">
+              Add profile section
+            </Button>
+            <Button className="proMore me-3" variant="outline-primary">
+              Add profile section
+            </Button>
+          </Form>
         </div>
       </div>
 
-      <div
-        style={{
-          overflow: "hidden",
-          height: "200px",
-          border: "solid 1px grey",
-        }}
-        className="d-flex flex-column align-items-start bg-light rounded-4 my-4 position-relative"
-      >
-        <div className="mt-5 mx-4">
-          <h3 className="mt-2 mb-1">Analitics</h3>
-          <p className="proGrey">
-            <AiFillEye />
+      <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
+        <div className="my-2 mx-4">
+          <h2 className="mt-2 mb-0">Analitics</h2>
+          <div className="proGrey proLight proSmall my-0">
+            <AiFillEye className="proIcon me-2" />
             private to you
-          </p>
-          <div className="d-flex">
-            <div className="proGrey me-5" style={{ fontSize: "15px", fontWeight:"500"}}>
-              <HiUsers style={{ fontSize: "25px", fontWeight:"500" }} className="me-2" /><span className="proGreyHBlue">{Math.floor(Math.random() * 100)} profile views </span><br /><span style={{ fontWeight: "300"}}>Discover who's viewed your profile.</span>
+          </div>
+          <div className="d-flex my-3">
+            <div className="proGrey proNormal me-5 d-flex">
+              <a href="#">
+                <HiUsers className="proGrey proIcon me-2" />
+              </a>
+              <div>
+                <a href="#">
+                  <span className="proGrey proGreyHBlue">
+                    {Math.floor(Math.random() * 100)} profile views{" "}
+                  </span>
+                </a>
+                <br />
+                <span className="proGrey proLight proSmall">
+                  Discover who's viewed your profile.
+                </span>
+              </div>
             </div>
-            <div className="proGrey me-5" style={{ fontSize: "15px", fontWeight:"500"}}>
-              <BiSearch style={{ fontSize: "25px", fontWeight:"500" }} className="me-2" /><span className="proGreyHBlue">{Math.floor(Math.random() * 100)} {Math.floor(Math.random() * 100)} search appearance</span><br /><span style={{ fontWeight: "300"}}>See how often you appear in search results</span>
-            </div>
-            <div className="proGrey"  style={{ fontSize: "15px", fontWeight:"500"}}>
-              <BiSearch style={{ fontSize: "25px" }} className="me-2" /> <span className="proGreyHBlue">{Math.floor(Math.random() * 100)} search appearance<br />See how often you appear in search results</span>
+            <div className="proGrey proNormal me-5 d-flex">
+              <a href="#">
+                <BiSearch className="proGrey proIcon me-2" />
+              </a>
+              <div>
+                <a href="#">
+                  <span className="proGrey proGreyHBlue">
+                    {Math.floor(Math.random() * 100)} search appearance
+                  </span>
+                </a>
+                <br />
+                <span className="proGrey proLight proSmall">
+                  See how often you appear in search results
+                </span>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div
-        style={{
-          overflow: "hidden",
-          height: "250px",
-          border: "solid 1px grey",
-        }}
-        className="d-flex flex-column align-items-start bg-light rounded-4 my-4 position-relative"
-      >
-        <div className="mt-5 mx-4">
-          <h3 className="mt-2 mb-1">About</h3>
-          <p className="text-secondary">{myProfile.bio}</p>
+      <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
+        <div className="my-4 mx-4">
+          <h2 className="my-0">About</h2>
+          <div className="proBlack prosmall proLight mt-2">{myProfile.bio}</div>
+        </div>
+      </div>
+      
+      <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
+        <div className="my-4 mx-4">
+          <h2 className="my-0">Experience</h2>
+          <a href="#">
+            <div className="proBlue my-0">
+              {Math.floor(Math.random() * 100)} connection
+            </div>
+          </a>
+          <div className="proBlack prosmall proLight mt-2">{myProfile.bio}</div>
         </div>
       </div>
 
-      <div
-        style={{
-          overflow: "hidden",
-          height: "150px",
-          border: "solid 1px grey",
-        }}
-        className="d-flex flex-column align-items-start bg-light rounded-4 my-4 position-relative"
-      >
-        <div className="mt-5 mx-4">
-          <h3 className="mt-2 mb-1">Activity</h3>
-          <p className="text-secondary">cose a caso</p>
+      <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
+        <div className="my-4 mx-4">
+          <h2 className="my-0">Activity</h2>
+          <a href="#">
+            <div className="proBlue my-0">
+              {Math.floor(Math.random() * 100)} connection
+            </div>
+          </a>
+          <div className="proBlack prosmall proLight mt-2">{myProfile.bio}</div>
         </div>
       </div>
 
       {/* <div>Experience</div> <-------- da fare domani */}
-      <div
-        style={{
-          overflow: "hidden",
-          height: "250px",
-          border: "solid 1px grey",
-        }}
-        className="d-flex flex-column align-items-start bg-light rounded-4 my-4 position-relative"
-      >
-        <div className="mt-5 mx-4">
-          <h3 className="mt-2 mb-1">interest</h3>
-          <p className="text-secondary">{myProfile.bio}</p>
+      <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
+        <div className="my-4 mx-4">
+          <h2 className="my-0">interest</h2>
+          <div className="proBlack prosmall proLight mt-2">{myProfile.bio}</div>
         </div>
       </div>
-      <div
-        style={{
-          overflow: "hidden",
-          height: "250px",
-          border: "solid 1px grey",
-        }}
-        className="d-flex flex-column align-items-start bg-light rounded-4 my-4 position-relative"
-      >
-        <div className="mt-5 mx-4">
-          <h3 className="mt-2 mb-1">Education</h3>
-          <p className="text-secondary">{myProfile.bio}</p>
+      <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
+        <div className="my-4 mx-4">
+          <h2 className="my-0">Education</h2>
+          <div className="proBlack prosmall proLight mt-2">{myProfile.bio}</div>
         </div>
       </div>
     </>
