66bc54fbadda7ea5397efacb7a5f833da4681f94
Eleonora Mattera <m.eleonora98@live.it>
2023-03-01 16:52:00 +0100

    post

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,27 +1,74 @@
-import { useEffect, useState } from "react"
-import { Button, Form, Modal } from "react-bootstrap"
-import { useDispatch, useSelector } from "react-redux"
-import { fetchProfile } from "../redux/actions"
-import { HiUsers } from "react-icons/hi"
-import { AiOutlineArrowRight, AiFillEye } from "react-icons/ai"
-import { BiSearch } from "react-icons/bi"
-import { FaSatelliteDish } from "react-icons/fa"
-import Exprience from "./Expercience"
-
+import { useEffect, useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProfile } from "../redux/actions";
+import { HiUsers } from "react-icons/hi";
+import { AiOutlineArrowRight, AiFillEye } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { FaSatelliteDish } from "react-icons/fa";
+import Exprience from "./Expercience";
+import { BiPencil } from "react-icons/bi";
+import { BsFillInfoSquareFill } from "react-icons/bs";
 const Profile = () => {
   /* MODALE*/
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const token = useSelector((state) => state.profile.token)
-  const myProfile = useSelector((state) => state.profile.profile)
-  const dispatch = useDispatch()
+  const token = useSelector((state) => state.profile.token);
+  const myProfile = useSelector((state) => state.profile.profile);
+  const dispatch = useDispatch();
+
+  //console.log("PROFILOOO", myProfile._id);
+  /*REDUCER PROFILEMODREDUCER DA USARE CON PUT*/
+  // const myProfileModified = useSelector((state) => state.profileModified.profileModified);
+  // console.log("USESELECTOR MYPROFILEMODIFIED", myProfileModified);
+
+  const obj = {
+    name: "",
+    surname: "",
+    email: "",
+    bio: "",
+    title: "",
+    area: "",
+  };
+  const [modified, setModified] = useState(null);
+  const [fetched, setFetched] = useState(false);
+
   useEffect(() => {
-    dispatch(fetchProfile(token))
-  }, [])
+    console.log("MODIFIED", modified);
+    //dispatch(changeProfile(myProfile._id, "PUT", obj));
+    async function ipipipip(id, expid) {
+      const urlToFetch = `https://striveschool-api.herokuapp.com/api/profile/`;
+      try {
+        const res = await fetch(urlToFetch, {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNmZhM2YxOTNlNjAwMTM4MDdmNTkiLCJpYXQiOjE2Nzc0ODg4MTYsImV4cCI6MTY3ODY5ODQxNn0.aQD1NJmhLvpzQEKvINIXWvlSMDQG-S49TU3R9DM5PWs`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(modified),
+        });
+        if (res.ok) {
+          console.log(res);
+          setFetched(true);
+          let profile = await res.json();
+          console.log("testPUT", profile);
+        } else {
+        }
+      } catch (error) {
+        alert(error);
+      }
+    }
+    ipipipip();
+  }, [modified]);
+
   // console.log(myProfile);
-  console.log("random number")
+  // console.log("random number");
+  useEffect(() => {
+    dispatch(fetchProfile(token));
+  }, [!fetched]);
+
   return (
     <>
       <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
@@ -42,135 +89,174 @@ const Profile = () => {
             alt="immagine profilo"
           />
         </a>
-        <div className="mt-5 mx-4">
-        
-        <h2 className="mt-2 mb-0">
-          {myProfile.name} {myProfile.surname}
-        </h2>
-        <div className="proBlack proMedium proLight">{myProfile.title}</div>
-        <a href="#">
-          <div className="proGrey proLight proGreyHBlue mt-2">{myProfile.email}</div>
-        </a>
-        <div className="mt-2">
+        <div className="mt-5 mx-4" style={{ position: "relative" }}>
+          <Button
+            className="matita"
+            style={{ position: "absolute", top: "-33px", left: "720px" }}
+            id="bottoneModale"
+            onClick={handleShow}>
+            <BiPencil />
+          </Button>
+
+          <h2 className="mt-2 mb-0">
+            {myProfile.name} {myProfile.surname}
+          </h2>
+          <div className="proBlack proMedium proLight">{myProfile.title}</div>
           <a href="#">
-            <span className="proGrey proLight proGreyHBlue">{myProfile.area}</span>
+            <div className="proGrey proLight proGreyHBlue mt-2">{myProfile.email}</div>
           </a>
+          <div className="mt-2">
+            <a href="#">
+              <span className="proGrey proLight proGreyHBlue">{myProfile.area}</span>
+            </a>
 
+            <a href="#">
+              <span className="proBlue">Contact info</span>
+            </a>
+          </div>
           <a href="#">
-            <span className="proBlue">Contact info</span>
+            <div className="proBlue mt-2 mb-3">{Math.floor(Math.random() * 100)} connection</div>
           </a>
-        </div>
-        <a href="#">
-          <div className="proBlue mt-2 mb-3">{Math.floor(Math.random() * 100)} connection</div>
-        </a>
-        <Form className="mb-4">
-          <Button className="proOpenTo me-3" variant="primary">
-            Open to
-          </Button>
-          <Button className="proModProfile me-3" variant="outline-primary">
-            Add profile section
-          </Button>
-          <Button className="proMore me-3" variant="outline-primary">
-            Add profile section
-          </Button>
-        </Form>
-
-        {/*MODALE*/}
-
-        <Button onClick={handleShow}>Launch demo modal</Button>
-        <Modal show={show} onHide={handleClose} style={{ height: "50%", position: "fixed", top: "30px" }}>
-          <Modal.Header closeButton className="fixed-top">
-            <Modal.Title className="fw-normal fs-5">Modifica introduzione</Modal.Title>
-          </Modal.Header>
-          <p className="text-secondary fw-light ms-3" style={{ fontSize: "12px" }}>
-            * Indica che il campo è obbligatorio
-          </p>
-          <Modal.Body style={{ overflowY: "scroll" }}>
-            <Form className="text-secondary">
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Nome*</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Cognome*</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Nome aggiuntivo*</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <p> Pronuncia del nome</p>
-              <p> Può essere aggiunta solo usando la nostra app per dispositivi mobili</p>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Inserisci pronomi personalizzati</Form.Label>
-                <Form.Control type="text" />
-                <Form.Text className="text-muted">
-                  Indica i pronomi di genere che vuoi che gli altri usino per riferirsi a te
-                </Form.Text>
-                <p> Scopri di più sui pronomi di genere</p>
-              </Form.Group>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Sommario*</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <h4> Posizione attuale</h4>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Posizione lavorativa*</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <div className="d-flex">
-                <Form.Check aria-label="option 1" />
-                <p>Mostra l'azienda attuale nella mia presentazione</p>
-              </div>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">settore*</Form.Label>
-                <Form.Control type="text" />
-                <p> Scopri di più sulle opzioni relative al settore</p>
-              </Form.Group>
-              <h4> Formazione</h4>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Formazione*</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <div className="d-flex">
-                <Form.Check aria-label="option 1" />
-                <p>Mostra l'azienda attuale nella mia presentazione</p>
-              </div>
-              <h4> Località</h4>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Paese/Area geografica*</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">CAP</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Città</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <h4> Informazioni di contatto</h4>
-              <p> Aggiungi o modifica il tuo profilo URL, indirizzo email e altro</p>
-
-              <p className="text-primary">Modifica le informazioni di contatto </p>
-              <h4> Sito Web</h4>
-              <Form.Text className="text-muted">
-                Aggiungi un link che apparirà nella parte superiore del tuo profilo
-              </Form.Text>
-              <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
-                <Form.Label className="mb-1">Link</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" className="rounded-5" onClick={handleClose}>
-              Salva
+          <Form className="mb-4">
+            <Button className="proOpenTo me-3" variant="primary">
+              Open to
             </Button>
-          </Modal.Footer>
-        </Modal>
+            <Button className="proModProfile me-3" variant="outline-primary">
+              Add profile section
+            </Button>
+            <Button className="proMore me-3" variant="outline-primary">
+              Add profile section
+            </Button>
+          </Form>
+
+          {/*MODALE*/}
+
+          {/* <Button onClick={handleShow}>
+            <BiPencil />
+          </Button>*/}
+          <Modal show={show} onHide={handleClose} style={{ height: "50%", position: "fixed", top: "30px" }}>
+            <Modal.Header closeButton>
+              <Modal.Title className="fw-normal fs-5">Modifica introduzione</Modal.Title>
+            </Modal.Header>
+            <p className="text-secondary fw-light ms-3" style={{ fontSize: "12px" }}>
+              * Indica che il campo è obbligatorio
+            </p>
+            <Modal.Body style={{ overflowY: "scroll" }}>
+              <Form className="text-secondary">
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Nome*</Form.Label>
+                  <Form.Control
+                    type="text"
+                    defaultValue={myProfile.name}
+                    onChange={(e) => (obj.name = e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Cognome*</Form.Label>
+                  <Form.Control
+                    type="text"
+                    defaultValue={myProfile.surname}
+                    onChange={(e) => (obj.surname = e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Nome aggiuntivo</Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+                <p style={{ fontSize: "14px" }}> Pronuncia del nome</p>
+                <p style={{ fontSize: "14px", color: "#535454" }}>
+                  <BsFillInfoSquareFill /> Può essere aggiunta solo usando la nostra app per dispositivi mobili
+                </p>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Inserisci pronomi personalizzati</Form.Label>
+                  <Form.Control type="text" />
+                  <Form.Text className="text-muted">
+                    Indica i pronomi di genere che vuoi che gli altri usino per riferirsi a te
+                  </Form.Text>
+                  <p> Scopri di più sui pronomi di genere</p>
+                </Form.Group>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Sommario*</Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+                <h5> Posizione attuale</h5>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Posizione lavorativa*</Form.Label>
+                  <Form.Control
+                    type="text"
+                    defaultValue={myProfile.title}
+                    onChange={(e) => (obj.title = e.target.value)}
+                  />
+                </Form.Group>
+                <div className="d-flex">
+                  <Form.Check aria-label="option 1" />
+                  <p>Mostra l'azienda attuale nella mia presentazione</p>
+                </div>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Settore*</Form.Label>
+                  <Form.Control
+                    type="text"
+                    defaultValue={myProfile.area}
+                    onChange={(e) => (obj.area = e.target.value)}
+                  />
+                  <p> Scopri di più sulle opzioni relative al settore</p>
+                </Form.Group>
+                <h5> Formazione</h5>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Formazione*</Form.Label>
+                  <Form.Control type="text" defaultValue={myProfile.bio} onChange={(e) => (obj.bio = e.target.value)} />
+                </Form.Group>
+                <div className="d-flex">
+                  <Form.Check aria-label="option 1" />
+                  <p>Mostra l'azienda attuale nella mia presentazione</p>
+                </div>
+                <h5> Località</h5>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Paese/Area geografica*</Form.Label>
+                  <Form.Control
+                    type="text"
+                    defaultValue={myProfile.area}
+                    onChange={(e) => (obj.area = e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Città</Form.Label>
+                  <Form.Control
+                    type="text"
+                    defaultValue={myProfile.area}
+                    onChange={(e) => (obj.area = e.target.value)}
+                  />
+                </Form.Group>
+                <h5 className="mt-4"> Informazioni di contatto</h5>
+                <p style={{ fontSize: "14px" }}> Aggiungi o modifica il tuo profilo URL, indirizzo email e altro</p>
+
+                <p className="text-primary fw-bold mt-4 mb-5">Modifica le informazioni di contatto </p>
+                <h5> Sito Web</h5>
+                <Form.Text className="text-muted">
+                  Aggiungi un link che apparirà nella parte superiore del tuo profilo
+                </Form.Text>
+                <Form.Group className="mb-3 text-secondary" style={{ fontSize: "14px" }}>
+                  <Form.Label className="mb-1">Link</Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" className="rounded-5" onClick={handleClose}>
+                CHIUDI
+              </Button>
+              <Button
+                variant="primary"
+                className="rounded-5"
+                onClick={() => {
+                  return setModified(obj), fetchProfile(token);
+                }}>
+                Salva
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
-        </div>
+      </div>
 
       <div className="d-flex flex-column align-items-start bg-light rounded-4 position-relative proCard my-4">
         <div className="my-2 mx-4">
@@ -253,7 +339,7 @@ const Profile = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
