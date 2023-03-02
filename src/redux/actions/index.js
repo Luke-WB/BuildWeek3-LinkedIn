4b88c37f61ce538ef0e12083d52edb4968c3b23f
Luca Scomazzon <scom.luca@gmail.com>
2023-03-02 01:18:28 +0100

    Fix type action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,5 @@
 export const CARICA_FETCH = "CARICA_FETCH";
+export const IS_LOADING = "IS_LOADING";
 export const SET_SHOW_EXP = "SET_SHOW_EXP";
 export const HOME_FETCH = "HOME_FETCH";
 
@@ -6,11 +7,6 @@ export const fetchProfile = (token) => {
   return async (dispatch, getState) => {
     const urlToFetch = "https://striveschool-api.herokuapp.com/api/profile/me";
     try {
-      // let res = await fetch(urlToFetch, {
-      //   headers: {'Authorization': `Berear eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjN2Y3MWYxOTNlNjAwMTM4MDdmNjAiLCJpYXQiOjE2Nzc0OTIwODEsImV4cCI6MTY3ODcwMTY4MX0.VsSZ2d0tCDoaQSZpm1CGnM4ctkdFFFZhAu36PvkG-hU
-      //   `},
-      // });
-
       const res = await fetch(urlToFetch, {
         headers: {
           Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNmZhM2YxOTNlNjAwMTM4MDdmNTkiLCJpYXQiOjE2Nzc0ODg4MTYsImV4cCI6MTY3ODY5ODQxNn0.aQD1NJmhLvpzQEKvINIXWvlSMDQG-S49TU3R9DM5PWs`,
@@ -24,22 +20,15 @@ export const fetchProfile = (token) => {
           payload: profile,
         });
       } else {
-        // dispatch({
-        //   type: HAS_ERROR,
-        // });
+        dispatch({
+          type: IS_LOADING,
+        });
       }
     } catch (error) {
       alert(error);
-      // dispatch({
-      //   type: IS_LOADING,
-      // });
-      // dispatch({
-      //   type: HAS_ERROR,
-      // });
     }
   };
 };
-
 
 export function fetchIdProfile(id) {
   const urlToFetch = `https://striveschool-api.herokuapp.com/api/profile/${id}`;
@@ -67,27 +56,26 @@ export function fetchIdProfile(id) {
 export function getExperiences(id) {
   const urlToFetch = `https://striveschool-api.herokuapp.com/api/profile/${id}/experiences`;
   return async (dispatch) => {
-     try {
-        const response = await fetch(urlToFetch, {
-          method: "GET",
-          headers: {
-            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNmZhM2YxOTNlNjAwMTM4MDdmNTkiLCJpYXQiOjE2Nzc0ODg4MTYsImV4cCI6MTY3ODY5ODQxNn0.aQD1NJmhLvpzQEKvINIXWvlSMDQG-S49TU3R9DM5PWs`,
-            'Content-Type': 'application/json'
-          },
-        })
-        if (response.ok) {
-           const data = await response.json();
-           console.log("getExp", data);
-           dispatch({type: "GET_EXPERIENCES", payload: data})
-        } else {
-           alert("Error fetching results");
-        }
-     } catch (error) {
-        console.log(error);
-     }
-  }
+    try {
+      const response = await fetch(urlToFetch, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNmZhM2YxOTNlNjAwMTM4MDdmNTkiLCJpYXQiOjE2Nzc0ODg4MTYsImV4cCI6MTY3ODY5ODQxNn0.aQD1NJmhLvpzQEKvINIXWvlSMDQG-S49TU3R9DM5PWs`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("getExp", data);
+        dispatch({ type: "GET_EXPERIENCES", payload: data });
+      } else {
+        alert("Error fetching results");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
-
 
 export const showModalExp = (toggleState) => {
   return {
@@ -107,24 +95,18 @@ export const fetchHome = (token) => {
       });
       if (res.ok) {
         let post = await res.json();
-        console.log("post",post);
+        console.log("post", post);
         dispatch({
           type: HOME_FETCH,
           payload: post,
         });
       } else {
-        // dispatch({
-        //   type: HAS_ERROR,
-        // });
+        dispatch({
+          type: IS_LOADING,
+        });
       }
     } catch (error) {
       alert(error);
-      // dispatch({
-      //   type: IS_LOADING,
-      // });
-      // dispatch({
-      //   type: HAS_ERROR,
-      // });
     }
   };
 };
