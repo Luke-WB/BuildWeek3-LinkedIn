81d1e923015dc74d0a634a31dd49fd0b9d1f7aa1
Luca Scomazzon <scom.luca@gmail.com>
2023-03-02 03:06:22 +0100

    Fix Modale nuovo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchHome, fetchProfile } from "../redux/actions";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import { MdPhotoSizeSelectActual } from "react-icons/md";
 import { BsFillPlayBtnFill, BsCalendarDay } from "react-icons/bs";
 import { MdArticle } from "react-icons/md";
 import HomeProfile from "./HomeProfile";
 import { Link } from "react-router-dom";
 import Loading from "./Loading";
+import Modal from "react-bootstrap/Modal";
 
 const Home = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const addPost = {
+    text: "",
+  };
+  const [objPost, setObjPost] = useState(addPost);
+  const handleChange = (field, value) => {
+    setObjPost((prev) => ({ ...prev, [field]: value }));
+  };
+  const [newPost, setPost] = useState([]);
   // profile fetch
   const token = useSelector((state) => state.profile.token);
   const loading = useSelector((state) => state.profile.loading);
@@ -23,6 +36,29 @@ const Home = () => {
   useEffect(() => {
     dispatch(fetchHome(token));
   }, []);
+
+  async function postPost() {
+    const urlToFetch = "https://striveschool-api.herokuapp.com/api/posts/";
+    try {
+      const res = await fetch(urlToFetch, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNmZhM2YxOTNlNjAwMTM4MDdmNTkiLCJpYXQiOjE2Nzc0ODg4MTYsImV4cCI6MTY3ODY5ODQxNn0.aQD1NJmhLvpzQEKvINIXWvlSMDQG-S49TU3R9DM5PWs`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(objPost),
+      });
+      if (res.ok) {
+        const addPost = await res.json();
+        console.log("testPOST", addPost);
+        setPost(addPost);
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
 
   return (
     <>
@@ -47,9 +83,29 @@ const Home = () => {
                       />
                     </Link>
                   </div>
-                  <span className="d-inline-block proMore proGrey position-relative postModBar me-3">
+                  <span
+                    className="d-inline-block proMore proGrey position-relative postModBar me-3"
+                    onClick={handleShow}
+                  >
                     <span className="postModText me-3">Start a post</span>
                   </span>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Create a post</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="d-flex flex-column">
+                      <Form.Control as="textarea" rows={3} onChange={(e) => handleChange("text", e.target.value)} />
+                      <Button
+                        variant="primary"
+                        onClick={() => {
+                          postPost();
+                          handleClose();
+                        }}
+                      >
+                        Save Changes
+                      </Button>
+                    </Modal.Body>
+                  </Modal>
                 </div>
                 <div className="d-flex justify-content-evenly my-2 mx-4">
                   <div className="greyHover rounded-2 me-2 px-2 py-3">
@@ -69,6 +125,35 @@ const Home = () => {
                   </div>
                 </div>
               </div>
+              {newPost.map((singPost, i) => {
+                return (
+                  <>
+                    <div
+                      key={i + "post"}
+                      className="d-flex flex-column align-items-start bg-light rounded-3 position-relative proCard my-4"
+                    >
+                      <div className="my-2 mx-4">
+                        <h3 className="proBlack my-2">
+                          writted by{" "}
+                          <Link to={`/user/${singPost.user?._id}`}>
+                            <span className="proBlack proGreyHBlue">{singPost.user?.name}</span>
+                          </Link>
+                        </h3>
+                        <div className="my-2 me-5">
+                          <span className="proGrey proBlack proLight proSmall proNormal">{singPost.text}</span>
+                        </div>
+                        <div className="proSmall proLight">edited: {singPost.updatedAt.slice(0, 10)}</div>
+                        <Button className="proModProfile me-3 my-3" variant="outline-primary">
+                          Add
+                        </Button>
+                        <Button className="proDelete" variant="danger">
+                          Delete
+                        </Button>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
               {post
                 .filter((postUnfilt, h) => h > 2800 && postUnfilt.text.length > 10)
                 .slice(0, 10)
