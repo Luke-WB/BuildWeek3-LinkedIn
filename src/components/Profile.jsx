d1992b673ed25ffa44de0d6063c80c5395a64c74
Luca Scomazzon <scom.luca@gmail.com>
2023-03-03 13:39:16 +0100

    Fix Link to

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -69,7 +69,7 @@ const Profile = () => {
     <>
       <div className="d-flex flex-column align-items-start bg-light rounded-3 position-relative proCard my-4">
         <div className="proImgBarSetting">
-          <Link to="/">
+          <Link to={"/"}>
             <img
               className="proImgBarSetting"
               style={{ objectFit: "cover", objectPosition: "top" }}
@@ -78,7 +78,7 @@ const Profile = () => {
             />
           </Link>
         </div>
-        <Link to="/">
+        <Link to={"/"}>
           <img className="rounded-circle position-absolute proAbsolute" src={myProfile.image} alt="immagine profilo" />
         </Link>
         <div className="mt-5 mx-4">
@@ -90,19 +90,19 @@ const Profile = () => {
             {myProfile.name} {myProfile.surname}
           </h2>
           <div className="proBlack proMedium proLight">{myProfile.title}</div>
-          <Link to="/">
+          <Link to={"/"}>
             <div className="proGrey proLight proGreyHBlue mt-2">{myProfile.email}</div>
           </Link>
           <div className="mt-2">
-            <Link to="/">
+            <Link to={"/"}>
               <span className="proGrey proLight proGreyHBlue">{myProfile.area}</span>
             </Link>
 
-            <Link to="/">
+            <Link to={"/"}>
               <span className="proBlue">Contact info</span>
             </Link>
           </div>
-          <Link to="/">
+          <Link to={"/"}>
             <div className="proBlue mt-2 mb-3">{Math.floor(Math.random() * 100)} connection</div>
           </Link>
           <Form className="mb-4">
@@ -254,11 +254,11 @@ const Profile = () => {
           </div>
           <div className="d-flex my-3">
             <div className="proGrey proNormal me-5 d-flex">
-              <Link to="/">
+              <Link to={"/"}>
                 <HiUsers className="proGrey proIcon me-2" />
               </Link>
               <div>
-                <Link to="/">
+                <Link to={"/"}>
                   <span className="proGrey proGreyHBlue">{Math.floor(Math.random() * 100)} profile views </span>
                 </Link>
                 <br />
@@ -266,11 +266,11 @@ const Profile = () => {
               </div>
             </div>
             <div className="proGrey proNormal me-5 d-flex">
-              <Link to="/">
+              <Link to={"/"}>
                 <BiSearch className="proGrey proIcon me-2" />
               </Link>
               <div>
-                <Link to="/">
+                <Link to={"/"}>
                   <span className="proGrey proGreyHBlue">{Math.floor(Math.random() * 100)} search appearance</span>
                 </Link>
                 <br />
@@ -292,7 +292,7 @@ const Profile = () => {
         <div className="my-4 mx-4">
           <div>
             <h2 className="my-0">Activity</h2>
-            <Link to="/">
+            <Link to={"/"}>
               <div className="proBlue my-0">{Math.floor(Math.random() * 100)} connection</div>
             </Link>
           </div>
