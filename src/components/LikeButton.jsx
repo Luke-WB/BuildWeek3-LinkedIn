771f361f6ac5fd2813f44d4d5b5cf76191b82261
Vallerga <antoniovallerga85@gmail.com>
2023-03-05 12:28:55 +0100

    Update LikeButton.jsx

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -2,57 +2,23 @@ import { useState } from "react";
 import { BsHandThumbsUp } from "react-icons/bs";
 
 const LikeButton = ({ indexButton }) => {
+  let cssClass = "greyHover rounded-2 me-2 px-4 py-3";
+  const [likeSwitch, setLikeSwitch] = useState(true);
 
-  let cssClass = "like";
+  if (likeSwitch) {
+    cssClass+= " likeActive";
+  }
 
-  const [likeSwitch, setLikeSwitch] = useState(false);
+  const handleClick = () => {
+    setLikeSwitch(!likeSwitch)
+  }
 
-  const setLikeUp = (comparator) => {
-
-    if (indexButton === comparator) {
-      console.log(`indexButton = ${indexButton} comparator = ${comparator}`);
-      setLikeSwitch(!likeSwitch);
-      if (likeSwitch) {
-        cssClass = "likeActive";
-      } else{
-        cssClass = "like";
-      }
-    } else {
-      console.log("indexButton === comparator = ", indexButton === comparator);
-    }
-  };
-
-  const consologga = () => console.log("consologga");
-
-  return (
-    <>
-      <div
-        onClick={(i) => setLikeUp(i)}
-        className={`greyHover ${cssClass} rounded-2 me-2 px-4 py-3`}
-      >
+  return (    
+      <div onClick={() => setLikeSwitch(!likeSwitch)} className={cssClass}>
         <BsHandThumbsUp className="fs-4 me-2 reverseChar" />
         Like
-      </div>
-    </>
+      </div>    
   );
 };
 
 export default LikeButton;
-
-// {likeSwitch ? (
-//   <div className="greyHover rounded-2 me-2 px-4 py-3">
-//     <BsHandThumbsUp
-//       onClick={() => setLikeUp()}
-//       className="fs-4 me-2 reverseChar"
-//     />
-//     Like
-//   </div>
-// ) : (
-//   <div className="greyHover rounded-2 me-2 px-4 py-3 likeOption">
-//     <BsHandThumbsUp
-//       onClick={() => setLikeUp()}
-//       className="likeOption fs-4 me-2 reverseChar"
-//     />
-//     Like
-//   </div>
-// )}
