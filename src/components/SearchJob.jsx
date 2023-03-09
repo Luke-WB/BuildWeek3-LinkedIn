5f2e2dc16552cabeab6a167e595093f07c428775
Luca Scomazzon <scom.luca@gmail.com>
2023-03-09 15:32:50 +0100

    FIx jobs

--- src/components/SearchJob.jsx
+++ src/components/SearchJob.jsx
@@ -40,19 +40,13 @@ https://strive-benchmark.herokuapp.com/api/jobs?category=writing&limit=10 //FETC
 
   return (
     <>
+      <Form className="d-flex">
+        <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" onChange={handleChange} />
+        <Button variant="outline-success" onClick={() => getJobs(jobs)}>
+          Search
+        </Button>
+      </Form>
       <div className="d-flex flex-column align-items-start bg-light rounded-3 position-relative proCard my-4">
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-            onChange={handleChange}
-          />
-          <Button variant="outline-success" onClick={() => getJobs(jobs)}>
-            Search
-          </Button>
-        </Form>
         <div className="my-4 mx-4">
           <h2 className="my-0">{capitalizeFirstLetter("Prova")}</h2>
           {jobsFetch.slice(0, 5).map((job) => {
